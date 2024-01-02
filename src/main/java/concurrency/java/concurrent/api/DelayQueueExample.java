8c42e9c4c75da9b906133c7bdb0f3c7ea296b081
Azat Satklichov <azat.satklichov@broadcom.com>
2024-01-02 11:18:45 +0100

    cleanup

--- src/main/java/concurrency/java/concurrent/api/DelayQueueExample.java
+++ src/main/java/concurrency/java/concurrent/api/DelayQueueExample.java
@@ -14,6 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class DelayQueueExample {
 
+	//https://jenkov.com/tutorials/java-util-concurrent/delayqueue.html
     public static void main(String[] args) throws InterruptedException {
         DelayQueue queue = new DelayQueue();
 
