4e650faf8e5fd6d94924a617d5bc94cad5ed1171
Azat Satklichov <azat.satklichov@broadcom.com>
2024-06-28 10:38:42 +0200

    elh all done regarding Concurrency 93 dereje bn

--- java-concurrent-programming-virtual-threads2/05/demos/05_Introducing-Structured-Concurrency/src/main/java/org/paumard/structuredconcurrency/E_travelandforecast/model/Page.java
+++
@@ -1,8 +0,0 @@
-package org.paumard.structuredconcurrency.E_travelandforecast.model;
-
-import org.paumard.structuredconcurrency.E_travelandforecast.model.travel.Travel;
-import org.paumard.structuredconcurrency.E_travelandforecast.model.weather.Weather;
-
-public record Page(Travel travel, Weather weather) {
-
-}
